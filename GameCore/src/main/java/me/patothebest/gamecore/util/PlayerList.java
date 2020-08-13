b02c5c940bacc81ef4d6b4adcb2cc5ce278ffee7
PatoTheBest <jpguereque@yahoo.com.mx>
2020-08-13 15:12:06 -0500

    Fix ConcurrentModificationException on PlayerList

--- GameCore/src/main/java/me/patothebest/gamecore/util/PlayerList.java
+++ GameCore/src/main/java/me/patothebest/gamecore/util/PlayerList.java
@@ -5,15 +5,15 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class PlayerList implements Set<Player> {
 
-    private final Map<Integer, Player> players = new HashMap<>();
+    private final Map<Integer, Player> players = new ConcurrentHashMap<>();
 
     @Override
     public int size() {
